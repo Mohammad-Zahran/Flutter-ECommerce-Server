6fb7f6e30f30a0eef50ab58f587b5509222dd8e5
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-01-30 20:25:23 +0200

    Establesh the connection inside index.js

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const asyncHandler = require('express-async-handler');
-const dotenv = require('dotenv');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const asyncHandler = require("express-async-handler");
+const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-app.use(cors({origin: '*'}))
+app.use(cors({ origin: "*" }));
 app.use(bodyParser.json());
+
+const URL = process.env.MONGO_URL;
+mongoose.connect(URL);
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Database"));
+
+app.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    res.json({
+      success: true,
+      message: "API working successfully",
+      data: null,
+    });
+  })
+);
+
+app.use((error, req, res, next) => {
+  res.status(500).json({ success: false, message: error.message, data: null });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
