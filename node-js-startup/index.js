6dc782066c67c3216994a4138ea75fa907cfcfc9
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-01-30 19:24:57 +0200

    Updated the versions of the dependencies

--- node-js-startup/index.js
+++ node-js-startup/index.js
@@ -1,7 +1,15 @@
-const express = require('express')
-const bodyParser = require('body-parse')
+const express = require("express");
+const bodyParser = require("body-parse");
 
+const app = express();
+const port = 8080;
 
+app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  res.json("Hello api is working");
+});
 
-
+app.listen(port, () => {
+  console.log(`Server is running on: ${port}`);
+});
