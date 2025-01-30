e30c55d167148b8173e24ec17e1f0146915e24a5
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-01-30 20:18:19 +0200

    resseting up the server side

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const asyncHandler = require('express-async-handler');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+app.use(cors({origin: '*'}))
+app.use(bodyParser.json());
