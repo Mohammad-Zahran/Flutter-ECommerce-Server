e3e6d99fd09c1d1d876406b0774fb718420c9200
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-02-17 16:16:43 +0200

    Create the UpdateProduct API

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -5,5 +5,6 @@ const productController = require("../controllers/product.controller.js");
 
 router.post("/", productController.CreateProduct);
 router.get("/", productController.getAllProducts);
+router.get("/:id", productController.getProductByID);
 
 module.exports = router;
